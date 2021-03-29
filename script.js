d218af9e40f6b4a303a7fcbb1cdb2c6b2c59038b
mklapshin <golub.97@mail.ru>
2021-03-29 20:41:44 +0300

    выполнил домашнее задание № 1

---
+++ script.js
@@ -0,0 +1,8 @@
+// Задание 1
+
+temperatureCelcius = prompt('Ввeдите температуру в градусах по Цельсию:');
+temperatureForengeit = (9 / 5) * temperatureCelcius + 32;
+alert(temperatureForengeit);
+
+// Задание 3
+// JS-выражение 1000 + "108" будет равно 1000108
