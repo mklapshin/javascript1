da132e0e6954645fcf499ba944540987f2bb10f0
mklapshin <golub.97@mail.ru>
2021-04-08 09:25:39 +0300

    выполнил домашнее задание № 3

--- script.js
+++ script.js
@@ -1,83 +1,43 @@
 // Задание 1
 
-// var a = 1, b = 1, c, d;
-// c = ++a; alert(c); // 2 // Префиксный инкремент - переменная а увеличивается на единицу и выводится конечное значение
-// d = b++; alert(d); // 1 // Постфиксный инкремент - переменная b увеличивается на единицу, но выводится предыдущее значение
-// c = (2+ ++a); alert(c); // 5 // Префиксный инкремент - переменная а увеличивается на 1 и к этому результату прибавляется число 2
-// d = (2+ b++); alert(d); // 4 // Постфиксный инкремент - переменная b увеличивается на 1, но выводится предыдущий результат этой переменной + 2
-// alert(a); // 3 // Выводится текущее значение переменной а, полученное 2 шага назад
-// alert(b); // 3 // Выводится текущее значение переменной b, полученное 2 шага назад
-
-// Задание 2
-// x будет равен 5
-
-// Задание 3
-
-countNumber();
-
-function countNumber() {
-    const b1 = prompt('Ввеите первое целое число');
-    const b2 = prompt('Ввеите второе целое число');
-    if (b1 >= 0 & b2 >= 0) {
-        c = b1 - b2;
-    } else if (b1 < 0 & b2 < 0) {
-        c = b1 * b2;
-    } else {
-        c = b1 + b2;
+let counter = 0;
+while (counter <= 100) {
+    if (isSimple(counter)) {
+        console.log(counter);
     }
-    alert(c);
-    return;
+    counter++;
 }
 
-// Задание 5
-
-function summNumbers(term1, term2) {
-    result = term1 + term2;
-    alert(result);
-    return;
-}
-
-function differenceNumbers(minuend, subtrahend) {
-    result = minuend - subtrahend;
-    alert(result);
-    return;
-}
-
-function multNumbers(factor1, factor2) {
-    result = factor1 * factor2;
-    alert(result);
-    return;
-}
-
-function divisionNumbers(dividend, divider) {
-    result = dividend / divider;
-    alert(result);
-    return;
-}
-
-// Задание 6
-
-const arg1 = parseInt(prompt('Введите значение arg1'));
-const arg2 = parseInt(prompt('Введите значение arg2'));
-let operation = prompt('Введите значение operation');
-
-mathOperation(arg1, arg2, operation);
-
-function mathOperation(value1, value2, chooseOperation) {
-    switch (chooseOperation) {
-        case '1':
-            summNumbers(value1, value2);
-            break;
-        case '2':
-            differenceNumbers(value1, value2);
-            break;
-        case '3':
-            multNumbers(value1, value2);
-            break;
-        case '4':
-            divisionNumbers(value1, value2);
-            break;
+function isSimple(number) {
+    if (number === 0) {
+        return false;
+    } 
+    if (typeof number !== 'number') {
+        return false;
     }
 
+    for(let divider = 2; divider < number; divider++) {
+        if (number % divider === 0) {
+            return false;
+        }
+    } 
+    return true;
+    }
+
+// Задания 2 и 3
+
+const internetShopGoods = ['молоко', 'хлеб', 'рыба', 'мясо', 'творог', 'сметана'];
+const internetShopPrices = [10, 20, 30, 40, 34, 26];
+
+function countBasketPrice(arrayOfItems, arrayOfPrices) {
+    let totalPrice = 0;
+    const counterOfItems = arrayOfItems.length;
+    let counterOfPrices = 0;
+    while (counterOfPrices < counterOfItems) {
+        totalPrice = totalPrice + arrayOfPrices[counterOfPrices];
+        counterOfPrices = counterOfPrices + 1;
+    }
+    alert('Стоимость корзины равна ' + totalPrice);
     return;
 }
+countBasketPrice(internetShopGoods, internetShopPrices);
